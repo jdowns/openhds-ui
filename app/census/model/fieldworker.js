f1d54319e8537edb7044ee8c3d75ae8ee396bc87
John <john.downs@acm.org>
2015-09-06 17:06:58 -0400

    authorize function for fieldworker

--- app/census/model/fieldworker.js
+++ app/census/model/fieldworker.js
@@ -5,8 +5,11 @@ angular.module('openHDS.model').factory('FieldWorkerService', FieldWorkerService
 FieldWorkerService.$inject = ['BackendService', 'ModelService'];
 
 function FieldWorkerService(BackendService, ModelService) {
-    return {foo: 1,
-        b: BackendService.someVariable,
-        c: ModelService.someVariable
+    return {
+        authorize: authorize,
     };
+
+    function authorize(username, password) {
+        return true;
+    }
 }
--- app/census/model/fieldworker_spec.js
+++ app/census/model/fieldworker_spec.js
@@ -12,15 +12,11 @@ describe('Fieldworker Service', function() {
             someVariable: 1
         });
         $provide.value('ModelService', {
-            someVariable: 2
         });
     }));
 
-    it('should authorize a valid user', inject(function(FieldWorkerService) {
-        expect(FieldWorkerService).toBeDefined();
-        expect(FieldWorkerService.foo).toEqual(1);
-        expect(FieldWorkerService.b).toEqual(1);
-        expect(FieldWorkerService.c).toEqual(2);
-
+    it('should authorize a valid user', inject(function(FieldWorkerService, BackendService, ModelService) {
+        expect(BackendService.someVariable).toEqual(1);
+        expect(FieldWorkerService.authorize('username', 'password')).toBe(true);
     }));
 });

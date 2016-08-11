455eab66f4316dde8054161f6e4fc6edb4ecf377
John Downs <john.downs@acm.org>
2016-08-10 21:43:54 -0400

    updating travis.yml and package info

--- karma.conf.js
+++ karma.conf.js
@@ -26,11 +26,12 @@ module.exports = function(config){
     browsers : ['Chrome'],
 
     plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-jasmine',
-            'karma-junit-reporter'
-            ],
+        'karma-chrome-launcher',
+        'karma-firefox-launcher',
+        'karma-phantomjs-launcher',
+        'karma-jasmine',
+        'karma-junit-reporter'
+    ],
 
     junitReporter : {
       outputFile: 'test_out/unit.xml',

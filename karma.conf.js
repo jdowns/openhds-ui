45f90ba3b7164641f23d489b35b9f8dc07041d6e
John Downs <john.downs@acm.org>
2016-09-30 21:21:00 -0400

    fixing karma tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,54 +1,36 @@
-module.exports = function(config){
-  config.set({
+module.exports = function(config) {
+    config.set({
+        basePath : './',
 
-      basePath : './',
+        plugins : [
+            'karma-phantomjs-launcher',
+            'karma-chrome-launcher',
+            'karma-junit-reporter',
+            'karma-jasmine'
+        ],
 
-      plugins : [
-          'karma-coverage',
-          'karma-junit',
-          'karma-babel-preprocessor',
-          'karma-chrome-launcher',
-          'karma-firefox-launcher',
-          'karma-phantomjs-launcher',
-          'karma-jasmine'
-      ],
+        files: [
+            'app/node_modules/angular/angular.js',
+            'app/node_modules/angular-route/angular-route.js',
+            'app/node_modules/angular-mocks/angular-mocks.js',
+            'app/index.js',
+            'app/auth/controller/*.js',
+            'app/auth/test/*.js'
+        ],
 
-      preprocessors: {
-          'ui/!(bower_components|npm_modules)/**/!(test)/*.js': 'babel',
-          'ui/!(bower_components|npm_modules)/**/!(test)/*.js': 'coverage'
-    },
+        frameworks: ['jasmine'],
+        browsers: ['PhantomJS'],
+        autoWatch: true,
+        reporters: ['progress', 'junit'],
 
-    files : [
-        'ui/bower_components/angular/angular.js',
-        'ui/bower_components/angular-route/angular-route.js',
-        'ui/bower_components/angular-mocks/angular-mocks.js',
-        'ui/test/framework.js',
-        'ui/app.js',
-
-        'ui/census/controller/*.js',
-        'ui/auth/controller/*.js',
-        'ui/update/controller/*.js',
-        'ui/audit/controller/*.js',
-
-        'ui/census/test/*.js',
-        'ui/auth/test/*.js',
-        'ui/update/test/*.js',
-        'ui/audit/test/*.js',
-        'ui/test/*.js'
-    ],
-
-      autoWatch : true,
-
-      frameworks: ['jasmine'],
-
-      browsers : ['PhantomJS'],
-
-
-      reporters: ['coverage', 'progress', 'junit'],
-      junitReporter : {
-          outputFile: 'test_out/unit.xml',
-          suite: 'unit'
-      }
-
-  });
+        junitReporter: {
+            outputDir: '',
+            outputFile: 'testresults.html',
+            suite: 'unit',
+            useBrowserName: true,
+            nameFormatter: undefined,
+            classNameFormatter: undefined,
+            properties: {}
+        }
+    });
 };

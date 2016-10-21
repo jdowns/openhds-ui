f7b3fe2c53e30fa81e697a29b02b3c497fae5143
Robert Tyler Miller <robert.miller@maine.edu>
2016-10-21 14:17:22 -0400

    update method skeletons

--- app/update/controller/update.js
+++ app/update/controller/update.js
@@ -10,10 +10,52 @@ function UpdateController($rootScope) {
 
     var vm = this;
 
+    vm.selectedLocation = null;
     vm.selectedIndividual = null;
     vm.submittedEvents = [];
 
 
     vm.currentInMigration = null;
+    vm.currentOutMigration = null;
+    vm.currentDeath = null;
+    vm.currentPregnancyObservation = null;
+    vm.currentPregnancyOutcome = null;
+
+    vm.submitInMigration = function(){
+        // post logic
+        // add to submitted events []
+        vm.currentInMigration = null;
+    };
+
+    vm.submitOutMigration = function(){
+        // post logic
+        // add to submitted events []
+        vm.currentOutMigration = null;
+    };
+
+    vm.submitDeath = function(){
+        // post logic
+        // add to submitted events []
+        vm.currentDeath = null;
+    };
+
+    vm.submitPregnancyObservation = function(){
+        // post logic
+        // add to submitted events []
+        vm.currentPregnancyObservation = null;
+    };
+
+    vm.submitPregnancyOutcome = function(){
+        // post logic
+        // add to submitted events []
+        vm.currentPregnancyOutcome = null;
+    };
+
+
+    vm.finishVisit = function(){
+        vm.submittedEvents = [];
+        vm.selectedLocation = null;
+        vm.selectedIndividual = null;
+    };
 
 }

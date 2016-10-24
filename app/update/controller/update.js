235450a998983c1b79653a1336c7d0e9b4c2f353
Robert Tyler Miller <robert.miller@maine.edu>
2016-10-24 13:04:46 -0400

    small ui adjustments

--- app/update/controller/update.js
+++ app/update/controller/update.js
@@ -46,6 +46,8 @@ function UpdateController($rootScope,
     vm.selectedIndividual = null;
     vm.submittedEvents = [];
 
+    vm.submittedVisits = [];
+
 
     vm.currentResidency = null;
     vm.currentInMigration = null;
@@ -68,7 +70,7 @@ function UpdateController($rootScope,
             .then(function(response) {
                 vm.currentVisit = response.data;
             });
-        $('#locationTab').tab('show');
+        $('#eventTab').tab('show');
     };
 
     vm.submitInMigration = function(event){
@@ -160,9 +162,17 @@ function UpdateController($rootScope,
 
 
     vm.finishVisit = function(){
+
+        vm.submittedVisits.push(vm.currentVisit);
+        vm.currentVisit = null;
+        vm.visit = null;
+
         vm.submittedEvents = [];
         vm.selectedLocation = null;
         vm.selectedIndividual = null;
+        $('#updateTab').tab('show');
+
+
     };
 
     vm.saveLocationHierarchy = function() {

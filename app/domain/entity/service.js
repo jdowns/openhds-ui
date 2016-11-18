25aaa0ef4f67a8ffca33cb43343f6196cd6cfd62
Robert Tyler Miller <robert.miller@maine.edu>
2016-11-18 17:06:37 -0500

    resolved visitDate toString issue

--- app/domain/entity/service.js
+++ app/domain/entity/service.js
@@ -108,9 +108,9 @@ function EntityService($rootScope, $http, $q) {
 
 
     service.getByVisitDate = function(urlBase, responseClass, visitDate) {
-        
 
-        var url = $rootScope.restApiUrl + urlBase + '/findByVisitDate?visitDate=' + visitDate;
+        var visJson = visitDate.toJSON();
+        var url = $rootScope.restApiUrl + urlBase + '/findByVisitDate?visitDate=' + visJson;
         var responsePromise = $http.get(url, service.getHeaders());
 
         return $q(function(resolve, reject) {

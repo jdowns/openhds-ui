49bd2a0684e99aade8101bc2c61e1b0e1cce4b3e
John Downs <john.downs@acm.org>
2016-12-10 19:47:51 -0500

    create new social group and assign memberships

--- test/membershipServiceSpec.js
+++ test/membershipServiceSpec.js
@@ -23,48 +23,4 @@ describe('MembershipService Test', function() {
         $httpBackend.verifyNoOutstandingRequest();
     });
 
-    it('Should post well formed data to rest api', function() {
-
-        $httpBackend.expectPOST(
-            'http://example.com/memberships',
-            {
-                collectedByUuid: '123',
-                individualUuid: 'indA',
-                socialGroupUuid: 'grp',
-                membership: {
-                    startType: 'UNIT TEST',
-                    startDate: 'then',
-                    collectionDateTime: 'nowish'
-                }
-            },
-            function(headers) {
-                return headers.authorization === 'Basic user:password';
-            }
-        ).respond(200, 'response one');
-
-
-        var model = {
-            currentFieldworker: {
-                uuid: '123'
-            },
-            collectionDateTime: 'nowish',
-            membership: {
-                individual: {uuid: 'indA'},
-                socialGroup: {uuid: 'grp'},
-                startType: 'UNIT TEST',
-                startDate: 'then'
-            }
-        };
-
-        var result = service.submit(model.currentFieldworker,
-                                    model.collectionDateTime,
-                                    model.membership);
-
-        Promise.all(result).then(function(response) {
-            //TODO: this is not executing correctly. It should fail
-            expect(response).toEqual('response one');
-        });
-
-        $httpBackend.flush();
-    });
 });
